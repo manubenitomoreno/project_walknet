476c339db7e9efbf7265f7afe2655e73d39415b7
manubenitomoreno <manubenitomoreno@gmail.com>
2022-09-03 13:06:09 +0200

    Upload modules for sources. Will contain the code from old repo

---
+++ sources/factory.py
@@ -0,0 +1,12 @@
+
+
+class Source:
+    keyname = self.keyname
+    provider = self.provider
+    table = self.table
+    #def inspect()
+    def run(level):
+        assert level in ['gather', 0,1,2], "specify a correct level to process"
+        if level
+    def upload()
+    def update()
